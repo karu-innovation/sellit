a0348c1777e72b3524982f515c76a4209e7ab7e6
retech <itsregalo047@gmail.com>
2022-04-23 10:15:56 +0300

    created db tables

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 
+
 User = settings.AUTH_USER_MODEL
 
 
@@ -10,22 +11,3 @@ class AccountType(models.Model):
 
     def __str__(self):
         return self.name
-
-class Product(models.Model):
-    account_type = models.ForeignKey(AccountType, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
-    is_active = models.BooleanField(default=True)
-    is_featured = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ['-id']
-        verbose_name_plural = 'Products'
-
-    def __str__(self):
-        return self.name
-
-    
