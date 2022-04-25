bdb2b51b58aab50c4f80418f0ca111f6b62c7933
retech <itsregalo047@gmail.com>
2022-04-25 17:38:34 +0300

    update

--- core/models.py
+++ core/models.py
@@ -11,3 +11,25 @@ class AccountType(models.Model):
 
     def __str__(self):
         return self.name
+
+class InstagramAccount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=254)
+    followers = models.PositiveIntegerField(blank=True, null=True)
+    lifespan = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    
+
+    def __str__(self):
+        return self.username
+    
+
+class InstagramAccountPhotos(models.Model):
+    account = models.ForeignKey(InstagramAccount, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='images/instagram_account/%Y/%m/%d')
+
+    def __str__(self):
+        return self.account.username
+    
--- core/views.py
+++ core/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import *
+
 
 # Create your views here.
 
 def IndexView(request, *args, **kwargs):
     return render(request, 'index.html')
+
+# utilizing the search func using Q
+def SearchView(request, *args, **kwargs):
+    query = request.GET.get('q', None)
+
+    if query is not None:
+        results = InstagramAccount.objects.filter(Q(username__icontains=query))
+
+    context = {
+        'results':results
+    }
+    return render(request, 'search-results.html', context)
